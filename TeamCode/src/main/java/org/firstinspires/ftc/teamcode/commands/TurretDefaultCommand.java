01241d55c3356e1d5f7e95753da660bb4fb2fee9
Ian N <156607232+IanNStealth4089@users.noreply.github.com>
2025-09-17 17:10:53 -0700

    Turret + Shooter
    Changes + Additions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TurretDefaultCommand.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.subsystems.TurretSubsystem;
+
+public class TurretDefaultCommand extends CommandBase {
+    TurretSubsystem turret;
+    public TurretDefaultCommand(TurretSubsystem turret){
+        this.turret = turret;
+        addRequirements(turret);
+    }
+    @Override
+    public void execute(){
+        turret.setPower(turret.pidCalc());
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretSubsystem.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class TurretSubsystem {
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.stealthrobotics.library.StealthSubsystem;
+
+public class TurretSubsystem extends StealthSubsystem {
+    private final DcMotorEx turretMotor;
+    private final PIDController pid;
+    private static double kP = 0;
+    private static double kI = 0;
+    private static double kD = 0;
+    private static double tolerance = 1;
+    public TurretSubsystem(HardwareMap hardwareMap){
+        turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
+        pid = new PIDController(kP,kI,kD);
+        pid.setTolerance(tolerance);
+    }
+    public void setPosition(double ticks){
+        pid.setSetPoint(ticks);
+    }
+    public void setPower(double power){
+        turretMotor.setPower(power);
+    }
+    public double getPosition(){
+        return turretMotor.getCurrentPosition();
+    }
+    public double pidCalc(){
+        return pid.calculate(getPosition());
+    }
 }
