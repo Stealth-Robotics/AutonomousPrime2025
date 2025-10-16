da62c49ce6e04f3b9bedb95a0d66a140773b25f9
Ian N <156607232+IanNStealth4089@users.noreply.github.com>
2025-10-15 18:36:01 -0700

    New Coordinate System

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
@@ -6,13 +6,16 @@ import android.util.Size;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
+import org.firstinspires.ftc.vision.apriltag.AprilTagMetadata;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.opencv.core.Mat;
 import org.stealthrobotics.library.StealthSubsystem;
@@ -26,9 +29,26 @@ public class VisionSubsystem extends StealthSubsystem {
 
     private final int MOTIF_GPP_ID = 21, MOTIF_PGP_ID = 22, MOTIF_PPG_ID = 23,
             GOAL_BLUE_ID = 20, GOAL_RED_ID = 24;
+    private VectorF poseAdjust(VectorF old){
+        return new VectorF(72 + old.getData()[1], 72 - old.getData()[0], old.getData()[2]);
+    }
+    private Quaternion angleAdjust(Quaternion q){
+        Quaternion rotation = new Quaternion(0.707f,0,0.707f,0,0);
+        q.multiply(rotation,0);
+        return(q);
+    }
 
     public VisionSubsystem(HardwareMap hardwareMap){
         //TODO: Add apriltag data
+        AprilTagLibrary.Builder libraryBuilder = new AprilTagLibrary.Builder();
+        AprilTagMetadata[] decodeLibrary = AprilTagGameDatabase.getDecodeTagLibrary().getAllTags();
+        for(AprilTagMetadata data : decodeLibrary){
+            if(data.fieldOrientation != null && data.fieldPosition != null) {
+                libraryBuilder.addTag(data.id, data.name, data.tagsize, poseAdjust(data.fieldPosition), data.distanceUnit, angleAdjust(data.fieldOrientation));
+            } else {
+                libraryBuilder.addTag(data);
+            }
+        }
         tagLibrary = new AprilTagLibrary.Builder()
                 .addTags(AprilTagGameDatabase.getDecodeTagLibrary())
                 .addTag(1,"Awesome",6.5, DistanceUnit.INCH)
