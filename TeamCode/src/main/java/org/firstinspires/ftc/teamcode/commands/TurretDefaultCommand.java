3d255b229d3623048d6c5c12f6ecb6dad7ed328e
Ian N <156607232+IanNStealth4089@users.noreply.github.com>
2025-10-27 17:51:44 -0700

    Default Turret Command
    a4wesome

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TurretDefaultCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TurretDefaultCommand.java
@@ -1,27 +1,36 @@
 package org.firstinspires.ftc.teamcode.commands;
 
 import com.arcrobotics.ftclib.command.CommandBase;
+import com.pedropathing.geometry.Pose;
 
+import org.firstinspires.ftc.teamcode.PoseTracker;
 import org.firstinspires.ftc.teamcode.subsystems.TurretSubsystem;
+import org.stealthrobotics.library.Alliance;
 
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 public class TurretDefaultCommand extends CommandBase {
     private final TurretSubsystem turret;
-    private final DoubleSupplier right;
-    private final DoubleSupplier left;
-    double targetAngle = 0;
+    private final Pose goalPose;
+    private double targetAngle = 0;
+    private final Pose BlueGoalPose = new Pose(-58.3727,-55.6425);
+    private final Pose RedGoalPose = new Pose(-58.3727, 55.6425);
 
-    public TurretDefaultCommand(TurretSubsystem turret, DoubleSupplier right, DoubleSupplier left) {
+    public TurretDefaultCommand(TurretSubsystem turret, Alliance alliance) {
         this.turret = turret;
-        this.right = right;
-        this.left = left;
+        if (alliance.equals(Alliance.BLUE)) {
+            goalPose = BlueGoalPose;
+        } else {
+            goalPose = RedGoalPose;
+        }
         addRequirements(turret);
     }
 
     @Override
     public void execute() {
-        turret.setPower((left.getAsDouble() - right.getAsDouble()) * 0.5);
+        Pose currentPose = PoseTracker.getEstimatedPose();
+        targetAngle = Math.atan2(goalPose.getY() - currentPose.getY(), goalPose.getX() - currentPose.getX());
+        turret.setTarget(targetAngle-currentPose.getHeading());
     }
 }
