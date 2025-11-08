ceba2b0a7441683fa1cb39cfd437dbada0d63b4d
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2025-11-07 17:50:17 -0800

    setup code for ov9281 camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -5,55 +5,27 @@ import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-import org.firstinspires.ftc.teamcode.commands.IntakeDefaultCommand;
-import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.SpindexerSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.VisionSubsystem;
 import org.stealthrobotics.library.opmodes.StealthOpMode;
 
 public class Teleop extends StealthOpMode {
     private GamepadEx driveGamepad;
     private GamepadEx operatorGamepad;
 
-    private DriveSubsystem drive;
-    private ShooterSubsystem shooter;
-    private IntakeSubsystem intake;
-    private SpindexerSubsystem spindexer;
-
-    private final Gamepad.RumbleEffect fullSpindexerRumbleEffect = new Gamepad.RumbleEffect.Builder()
-            .addStep(0.0, 1.0, 500)  //  Rumble right motor 100% for 500 mSec
-            .addStep(0.0, 0.0, 300)  //  Pause for 300 mSec
-            .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
-            .addStep(0.0, 0.0, 250)  //  Pause for 250 mSec
-            .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
-            .build();
+    private VisionSubsystem vision;
 
     @Override
     public void initialize() {
         driveGamepad = new GamepadEx(gamepad1);
         operatorGamepad = new GamepadEx(gamepad2);
 
-        drive = new DriveSubsystem(hardwareMap);
-        shooter = new ShooterSubsystem(hardwareMap);
-        intake = new IntakeSubsystem(hardwareMap);
-        spindexer = new SpindexerSubsystem(hardwareMap);
-
-        register(drive, shooter, intake, spindexer);
-
-        driveGamepad.gamepad.runRumbleEffect(fullSpindexerRumbleEffect);
-        driveGamepad.gamepad.runLedEffect(new Gamepad.LedEffect.Builder().addStep(1, 0, 0, 1000).build());
-
-        //Setup default commands
-        intake.setDefaultCommand(new IntakeDefaultCommand(intake, () -> (driveGamepad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) - driveGamepad.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER))));
-        drive.setDefaultCommand(drive.driveTeleop(() -> driveGamepad.getLeftX(), () -> driveGamepad.getLeftY(), () -> driveGamepad.getRightX()));
+        vision = new VisionSubsystem(hardwareMap);
 
         //Configure gamepad bindings
         configureBindings();
     }
 
     private void configureBindings() {
-        driveGamepad.getGamepadButton(GamepadBindings.DriverBindings.RESET_HEADING).whenPressed(() -> drive.resetHeading());
     }
 
     @SuppressWarnings("unused")
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import android.util.Size;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -24,89 +25,49 @@ import org.stealthrobotics.library.StealthSubsystem;
 import static org.stealthrobotics.library.opmodes.StealthOpMode.telemetry;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @SuppressWarnings("FieldCanBeLocal")
+@Config
 public class VisionSubsystem extends StealthSubsystem {
     private final AprilTagProcessor aprilTagProcessor;
-
-    //TODO: VERY IMPORTANT TO SET THIS ACCURATELY
-    private final Position cameraPosition = new Position(DistanceUnit.INCH, 0, 0, 0, 0);
-    private final YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES, 0, -90, 0, 0);
+    private final VisionPortal visionPortal;
 
     private final int MOTIF_GPP_ID = 21, MOTIF_PGP_ID = 22, MOTIF_PPG_ID = 23;
     private final int GOAL_BLUE_ID = 20, GOAL_RED_ID = 24;
 
     public VisionSubsystem(HardwareMap hardwareMap) {
-        AprilTagLibrary.Builder libraryBuilder = new AprilTagLibrary.Builder();
-        AprilTagMetadata[] decodeLibrary = AprilTagGameDatabase.getDecodeTagLibrary().getAllTags();
-
-        for (AprilTagMetadata data : decodeLibrary) {
-            if (data.fieldOrientation != null && data.fieldPosition != null) {
-                libraryBuilder.addTag(
-                        data.id, data.name, data.tagsize, data.fieldPosition, data.distanceUnit, data.fieldOrientation
-                );
-            }
-            else libraryBuilder.addTag(data);
-        }
-
-        AprilTagLibrary tagLibrary = libraryBuilder.build();
-
         aprilTagProcessor = new AprilTagProcessor.Builder()
-                .setCameraPose(cameraPosition, cameraOrientation)
-                .setTagLibrary(tagLibrary)
+                .setTagLibrary(AprilTagGameDatabase.getDecodeTagLibrary())
                 .setDrawTagID(true)
                 .setDrawTagOutline(true)
                 .setDrawAxes(true)
                 .setDrawCubeProjection(true)
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                .setLensIntrinsics(549.651, 549.651, 317.108, 236.644) //From FTC discord (veer)
                 .build();
 
-        VisionPortal visionPortal = new VisionPortal.Builder()
-                .setCamera(hardwareMap.get(WebcamName.class, "aprilTagCamera"))
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
                 .addProcessor(aprilTagProcessor)
                 .setCameraResolution(new Size(640, 480))
-                .setStreamFormat(VisionPortal.StreamFormat.YUY2)
-                .setAutoStopLiveView(true)
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
                 .build();
-
-        if (FtcDashboard.getInstance().isEnabled())
-            FtcDashboard.getInstance().startCameraStream(visionPortal, 0);
-    }
-
-    //Converts from FTCCoordinates to PedroCoordinates
-    private Pose poseAdjust(Pose old) {
-        return new Pose(72 + old.getY(), 72 - old.getX(), old.getHeading() - (Math.PI / 2));
-    }
-
-    // ? Returns the new motif if it has changed since last call
-    public Motif.MotifType getUpdatedMotif(Motif.MotifType old) {
-        ArrayList<AprilTagDetection> latestDetections = aprilTagProcessor.getDetections();
-
-        for (AprilTagDetection detection : latestDetections) {
-            if (detection.id == MOTIF_PPG_ID && old != Motif.MotifType.PPG)
-                return Motif.MotifType.PPG;
-            else if (detection.id == MOTIF_GPP_ID && old != Motif.MotifType.GPP)
-                return Motif.MotifType.GPP;
-            else if (detection.id == MOTIF_PGP_ID && old != Motif.MotifType.PGP)
-                return Motif.MotifType.PGP;
-        }
-
-        return old; //No new motif found
     }
 
     @Override
     public void periodic() {
+        telemetry.addData("fps", visionPortal.getFps());
         ArrayList<AprilTagDetection> latestDetections = aprilTagProcessor.getDetections();
-        if (!latestDetections.isEmpty()) {
-            for (AprilTagDetection detection : latestDetections) {
-                if (detection.id == GOAL_BLUE_ID || detection.id == GOAL_RED_ID) {
-                    PoseTracker.updateEstimatedPose(poseAdjust(new Pose(
-                            detection.robotPose.getPosition().x,
-                            detection.robotPose.getPosition().y,
-                            detection.robotPose.getOrientation().getYaw(AngleUnit.RADIANS)
-                    )), false);
-                }
-            }
+        ArrayList<Integer> tagIds = new ArrayList<>();
+        for (AprilTagDetection detection : latestDetections) {
+            tagIds.add(detection.id);
         }
-        telemetry.addData("Detected Motif", Motif.getMotif());
+        telemetry.addData("tagIds", tagIds);
+        if (tagIds.contains(24)) {
+            AprilTagDetection tag = latestDetections.get(0);
+            telemetry.addData("range", tag.ftcPose.range);
+            telemetry.addData("headingOffset", tag.ftcPose.bearing);
+        }
     }
 }
