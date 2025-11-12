cad38b107b8b5c1cbe7685187253edd6bd1a3aa2
Ian N <156607232+IanNStealth4089@users.noreply.github.com>
2025-11-12 15:44:47 -0800

    i did some bads

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseSupplier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseSupplier.java
@@ -2,18 +2,22 @@ package org.firstinspires.ftc.teamcode;
 
 import com.pedropathing.geometry.Pose;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
 import java.util.function.DoubleSupplier;
 
 public class PoseSupplier {
-    DoubleSupplier x;
-    DoubleSupplier y;
-    DoubleSupplier heading;
+    private final DoubleSupplier x;
+    private final DoubleSupplier y;
+    private final DoubleSupplier heading;
     public PoseSupplier(DoubleSupplier x, DoubleSupplier y, DoubleSupplier heading) {
         this.x = x;
         this.y = y;
         this.heading = heading;
     }
-    public Pose getAsPose() {
-        return new Pose(x.getAsDouble(), y.getAsDouble(), heading.getAsDouble());
+    public Pose2D getAsPose() {
+        return new Pose2D(DistanceUnit.INCH, x.getAsDouble(), y.getAsDouble(), AngleUnit.DEGREES, heading.getAsDouble());
     }
 }
