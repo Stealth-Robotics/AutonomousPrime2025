615a1d3b910d10b7b80f91ba29beaa65562628e7
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2025-11-13 01:39:23 -0800

    sike, I had to update TestAuto with new subsystem parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAuto.java
@@ -1,39 +1,49 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.arcrobotics.ftclib.command.Command;
-import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
-import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.commands.SaveSubsystemData;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.PoseSupplier;
 import org.firstinspires.ftc.teamcode.pedroPathing.AlliancePoseFlipper;
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-import org.firstinspires.ftc.teamcode.Motif;
-import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.FollowerSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.LimelightSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.SpindexerSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.TurretSubsystem;
 import org.stealthrobotics.library.Alliance;
 import org.stealthrobotics.library.opmodes.StealthOpMode;
 
 public class TestAuto extends StealthOpMode {
+    private FollowerSubsystem follower;
+
+    private ShooterSubsystem shooter;
+    private IntakeSubsystem intake;
+    private TurretSubsystem turret;
+    private SpindexerSubsystem spindexer;
     private LimelightSubsystem limelight;
-    private Follower follower;
 
     private final Pose startPose = new Pose(0, 0, Math.toRadians(0));
 
     @Override
     public void initialize() {
-        limelight = new LimelightSubsystem(hardwareMap);
-        follower = Constants.createFollower(hardwareMap);
+        follower = new FollowerSubsystem(hardwareMap);
 
-        follower.setStartingPose(startPose);
+        shooter = new ShooterSubsystem(hardwareMap);
+        intake = new IntakeSubsystem(hardwareMap);
+        turret = new TurretSubsystem(hardwareMap, new PoseSupplier(() -> follower.getPose().getX(), () -> follower.getPose().getY(), () -> AngleUnit.RADIANS.toDegrees(follower.getPose().getHeading())));
+        spindexer = new SpindexerSubsystem(hardwareMap, intake);
+        limelight = new LimelightSubsystem(hardwareMap);
 
         //Add all poses in the autonomous here
         AlliancePoseFlipper.flip(Alliance.get(), new Pose[]{
                 startPose
         });
+
+        follower.setStartingPose(startPose);
     }
 
     @Override
