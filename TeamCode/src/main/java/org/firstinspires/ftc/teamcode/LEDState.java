c649ec776f07955552cb71d28ccc9a2fadc9b9c9
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2025-11-17 22:21:00 -0800

    Added led subsystem (still needs to be integrated once it arrives)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LEDState.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+public enum LEDState {
+    OFF(0.0),
+    RED(0.277),
+    ORANGE(0.333),
+    YELLOW(0.388),
+    SAGE(0.444),
+    GREEN(0.5),
+    AZURE(0.555),
+    BLUE(0.611),
+    INDIGO(0.666),
+    VIOLET(0.722),
+    WHITE(1.0);
+
+    private final double colorValue;
+
+    LEDState(double colorValue) {
+        this.colorValue = colorValue;
+    }
+
+    public double getColorValue() {
+        return colorValue;
+    }
+}
