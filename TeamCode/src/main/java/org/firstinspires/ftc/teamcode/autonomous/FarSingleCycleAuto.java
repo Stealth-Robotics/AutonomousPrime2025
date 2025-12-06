28d04435845ae7a27aa3a953fafefe786b511197
Ian N <156607232+IanNStealth4089@users.noreply.github.com>
2025-12-05 16:31:37 -0800

    Autos Galore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FarSingleCycleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FarSingleCycleAuto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes;
+package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
@@ -12,48 +12,43 @@ import org.firstinspires.ftc.teamcode.systems.RobotSystem;
 import org.stealthrobotics.library.commands.EndOpModeCommand;
 import org.stealthrobotics.library.opmodes.StealthOpMode;
 
-import org.firstinspires.ftc.teamcode.autonomous.AutoBuilder;
-
-public class TestAuto extends StealthOpMode {
-    private AutoBuilder builder;
+public class FarSingleCycleAuto extends StealthOpMode {
     private FollowerSubsystem follower;
+    private AutoBuilder builder;
     private RobotSystem robot;
-
     private Pose startPose;
 
     @Override
-    public void initialize() {
+    public void initialize(){
         follower = new FollowerSubsystem(hardwareMap);
         robot = new RobotSystem(hardwareMap);
-
         builder = new AutoBuilder(robot, follower);
-        startPose = builder.FAR_START_POSE;
 
-        //Reset pinpoint
+        startPose = builder.FAR_START_POSE;
         robot.drive.resetPosAndIMU();
 
-        //For localization
         robot.drive.setPose(startPose);
         follower.setStartingPose(startPose);
     }
 
     @Override
-    public Command getAutoCommand() {
+    public Command getAutoCommand(){
         return new SequentialCommandGroup(
-                // PUT AUTONOMOUS SEQUENCE HERE
                 new AutonomousShootCommand(robot),
+                builder.cycle(AutoBuilder.preset.FAR,false,false),
+                new AutonomousShootCommand(robot),
+                builder.parkFar(),
                 new SaveSubsystemData(robot),
                 new EndOpModeCommand(this)
         );
     }
-
     @SuppressWarnings("unused")
-    @Autonomous(name = "RedTestAuto", group = "Red")
-    public static class RedTestAuto extends TestAuto {
+    @Autonomous(name = "RedFarSingleCycle", group = "Red")
+    public static class RedFarSingleCycleAuto extends FarSingleCycleAuto {
     }
 
     @SuppressWarnings("unused")
-    @Autonomous(name = "BlueTestAuto", group = "Blue")
-    public static class BlueTestAuto extends TestAuto {
+    @Autonomous(name = "BlueFarSingleCycle", group = "Blue")
+    public static class BlueFarSingleCycleAuto extends FarSingleCycleAuto {
     }
 }
